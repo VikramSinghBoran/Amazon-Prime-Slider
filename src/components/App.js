9f24ad3188496bf8f4fc55ba52fe58e282c540ca
Vikram Singh Boran <vikramsinghboran98@gmail.com>
2022-02-21 23:23:18 +0530

    functioning provided

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,47 @@
-import React, {Component, useState} from "react";
+import React, {Component, useEffect, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
+
+
+// console.log(count + "from the top");
+
+  // console.log("increasing " + count);
+  // console.log(props);
+
+const App = (props) => {
+  const [count,setcount] = useState(0);
+  
+  const increment = () =>{
+    
+    if(count < 4){
+      setcount(1+count);
+      // console.log(this);
+    }
+    // console.log(count);
+  }
+
+  const decrement = () =>{
+    if(count > 0){
+      setcount(count-1);
+      // console.log(count);
+    }
+    
+  }
+
+  const reset = () =>{
+    setcount(0);
+    // console.log(count);
+  }
+
   return (
-    <></>
+    <>
+      <h1 data-testid="title">{props.slides[count].title}</h1>
+      <p data-testid="text">{props.slides[count].text}</p>  
+      <button data-testid="button-prev" onClick={decrement}>Prev</button>    
+      <button data-testid="button-next" onClick={increment}>Next</button>
+      <button data-testid="button-restart" onClick={reset}>Restart</button>
+          
+    </>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import slides from './data.js';
 
-// ReactDOM.render(<App slides={slides}/>, document.getElementById('root'));
+ReactDOM.render(
+    <App slides={slides}/>,
+    document.getElementById('root')
+);
